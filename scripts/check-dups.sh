8696779a4ef81de01cbdb2c736f043fc50a2ecc6
edoardottt <edoardott@gmail.com>
2023-04-12 12:15:59 +0200

    Add scripts

---
+++ scripts/check-dups.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+readme="README.md"
+
+links=$(cat $readme | egrep "\- \[" | wc -l)
+
+uniqlinks=$(cat $readme | egrep "\- \[" | uniq | wc -l)
+
+if [[ $links -eq $uniqlinks ]];
+then
+    echo "NO DUPLICATES FOUND."
+else
+    echo "DUPLICATES FOUND!"
+    cat $readme | egrep "\- \[" | uniq -c | egrep -iv "1 - ["
+fi
